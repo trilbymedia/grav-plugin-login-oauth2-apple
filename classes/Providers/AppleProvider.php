5a65785f27519eb3ffd06d8a429320601b18ecae
Andy Miller <rhuk@mac.com>
2022-12-27 17:50:16 -0700

    workaround for post-based callback and lax cookies

--- classes/Providers/AppleProvider.php
+++ classes/Providers/AppleProvider.php
@@ -2,28 +2,28 @@
 namespace Grav\Plugin\Login\OAuth2\Providers;
 
 use Grav\Common\Grav;
+use Grav\Common\Session;
+use League\OAuth2\Client\Provider\Apple;
+use League\OAuth2\Client\Provider\AppleResourceOwner;
+use League\OAuth2\Client\Provider\ResourceOwnerInterface;
 
 class AppleProvider extends BaseProvider
 {
     protected $name = 'Apple';
-    protected $classname = 'League\\OAuth2\\Client\\Provider\\Apple';
-    protected $config;
+    protected $classname = Apple::class;
+    protected $temp_cookie_name = 'siwa-temp-cookie';
 
-    /** @var Apple */
-    protected $provider;
-
-    public function initProvider(array $options)
+    public function initProvider(array $options): void
     {
-        $this->config = Grav::instance()['config'];
-        xdebug_break();
-        $keyFileId = $this->config->get('plugins.login-oauth2-apple.keyFileId');
-        $keyPath = $this->config->get('plugins.login-oauth2-apple.keyFilePath');
+        $config = Grav::instance()['config'];
+        $keyFileId = $config->get('plugins.login-oauth2-apple.keyFileId');
+        $keyPath = $config->get('plugins.login-oauth2-apple.keyFilePath');
         $realKeyPath = Grav::instance()['locator']->findResource($keyPath);
         $realKeyPath .= '/AuthKey_' . $keyFileId . '.p8';
 
         $options += [
-            'clientId'      => $this->config->get('plugins.login-oauth2-apple.client_id'),
-            'teamId'        => $this->config->get('plugins.login-oauth2-apple.team_id'),
+            'clientId'      => $config->get('plugins.login-oauth2-apple.client_id'),
+            'teamId'        => $config->get('plugins.login-oauth2-apple.team_id'),
             'keyFileId'     => $keyFileId,
             'keyFilePath'   => $realKeyPath
         ];
@@ -33,19 +33,69 @@ class AppleProvider extends BaseProvider
 
     public function getAuthorizationUrl()
     {
-        $options = ['state' => $this->state];
+        $options = ['state' => $this->getState()];
         $options['scope'] = $this->config->get('plugins.login-oauth2-apple.options.scope');
+
+        // workaround for Apple's crappy OAuth2 implementation that uses a POST callback that doesn't forward lax-cookie
+        $this->setTempCookie($options);
 
         return $this->provider->getAuthorizationUrl($options);
     }
 
-    public function getUserData($user)
+    public function getState(): string
     {
-        return [
+        return 'APPLE__' . $this->state;
+    }
+
+
+    public function getUserData(ResourceOwnerInterface $user): array
+    {
+        \assert($user instanceof AppleResourceOwner);
+
+        $full_name = trim($user->getFirstName() . " " . $user->getLastName());
+
+        $data = [
             'id'         => $user->getId(),
-            'login'      => $user->getAttribute('name'),
-            'fullname'   => $user->getAttribute('name'),
+            'login'      => $user->getEmail(),
             'email'      => $user->getEmail()
         ];
+
+        if (!empty($full_name)) {
+            $data['fullname'] = $full_name;
+        }
+
+        return $data;
+    }
+
+    public static function getCallbackUri(string $admin = 'auto'): string
+    {
+        $callback_uri = parent::getCallbackUri($admin);
+        $callback_uri .= '?XDEBUG_SESSION_START=1';
+        return $callback_uri;
+    }
+
+    public function setTempCookie(Array $options): void
+    {
+        // Create short-lived cookie to get around Apple's crappy OAuth2 implementation
+        $temp_data = [
+            'state' => $options['state'],
+            'redirect_after_login' => '/en/bookmarks', //testing only
+        ];
+
+        setcookie($this->temp_cookie_name, json_encode($temp_data), [
+            'expires' => time() + 1800,
+            'path' => '/',
+            'secure' => true,
+            'httponly' => true,
+            'samesite' => 'None',
+        ]);
+    }
+
+    public function getTempCookieData(): array
+    {
+        $temp_data = json_decode($_COOKIE[$this->temp_cookie_name] ?? [], true);
+        unset($_COOKIE[$this->temp_cookie_name]);
+        setcookie($this->temp_cookie_name, null, -1, '/');
+        return $temp_data;
     }
 }
--- login-oauth2-apple.php
+++ login-oauth2-apple.php
@@ -2,6 +2,9 @@
 namespace Grav\Plugin;
 
 use Grav\Common\Plugin;
+use Grav\Common\Utils;
+use Grav\Plugin\Login\OAuth2\ProviderFactory;
+use Grav\Plugin\Login\OAuth2\Providers\AppleProvider;
 use RocketTheme\Toolbox\Event\Event;
 
 /**
@@ -39,10 +42,16 @@ class LoginOAuth2ApplePlugin extends Plugin
     public function onPluginsInitialized()
     {
         if (isset($this->grav['oauth2'])) {
-            $this->grav['oauth2']->addProvider('apple');
+            $options = $this->config->get('plugins.login-oauth2-apple');
+            $this->grav['oauth2']->addProvider('apple', $options);
         } else {
             $this->grav['messages']->add('oauth2-apple plugin requires oauth2 plugin but it appears to not be installed or enabled', 'error');
         }
+
+        $this->enable([
+            'onTask.callback.oauth2'    => ['loginCallback', 100],
+            ]
+        );
 
     }
 
@@ -78,6 +87,35 @@ class LoginOAuth2ApplePlugin extends Plugin
         // add CSS for frontend if required
         if (!$this->isAdmin() && $this->config->get('plugins.login-oauth2-apple.built_in_css')) {
             $this->grav['assets']->add('plugin://login-oauth2-apple/css/login-oauth2-apple.css');
+        }
+    }
+
+    /**
+     * Worksaround to handle callback being called outside user's session
+     *
+     * @param Event $e
+     * @return void
+     */
+    public function loginCallback(Event $e)
+    {
+        $state = $_POST['state'] ?? null;
+        $code = $_POST['code'] ?? null;
+        if ($code && $state && Utils::startsWith($state, 'APPLE__')) {
+            $oauth2 = $this->grav['oauth2'];
+            $provider_name = 'apple';
+
+            /** @var AppleProvider $provider */
+            $provider = ProviderFactory::create($provider_name, $oauth2->getProviderOptions($provider_name));
+            $temp_data = $provider->getTempCookieData();
+            $temp_state = $temp_data['state'] ?? null;
+            $temp_redirect = $temp_data['redirect_after_login'] ?? null;
+
+            if ($temp_state === $state) {
+                $session = $this->grav['session'];
+                $session->oauth2_provider = $provider_name;
+                $session->oauth2_state = $state;
+                $session->oauth2_redirect = $temp_redirect;
+            }
         }
     }
 }
